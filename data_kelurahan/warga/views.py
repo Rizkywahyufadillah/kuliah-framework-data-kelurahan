acd91b656ef269bfb779ccb6caf04eaa47c9c511
Rizky Wahyu <rizkywahyufadillah3@gmail.com>
2025-10-20 21:51:45 +0700

    Menambahkan CRUD pengaduan dan Warga

--- data_kelurahan/warga/views.py
+++ data_kelurahan/warga/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView  
-from django.views.generic import DetailView  
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Warga,Pengaduan
+from .forms import WargaForm,PengaduanForm
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -16,3 +17,38 @@ class WargaDetailView(DetailView):
 class PengaduanListView(ListView):
     model = Pengaduan
     template_name = 'warga/pengaduan_list.html'
+
+class WargaCreateView(CreateView):
+    model = Warga
+    form_class = WargaForm
+    template_name = 'warga/warga_form.html'
+    success_url = reverse_lazy('warga-list')
+
+class PengaduanCreateView(CreateView):
+    model = Pengaduan
+    form_class = PengaduanForm
+    template_name = 'warga/pengaduan_form.html'
+    success_url = reverse_lazy('pengaduan-list')
+
+class WargaUpdateView(UpdateView):
+    model = Warga
+    form_class = WargaForm
+    template_name = 'warga/warga_form.html'
+    success_url = reverse_lazy('warga-list')
+
+class WargaDeleteView(DeleteView):
+    model = Warga
+    template_name = 'warga/warga_confirm_delete.html'
+    success_url = reverse_lazy('warga-list')
+
+class PengaduanUpdateView(UpdateView):
+    model = Pengaduan
+    form_class = PengaduanForm
+    template_name = 'warga/pengaduan_form.html'
+    success_url = reverse_lazy('pengaduan-list')
+
+
+class PengaduanDeleteView(DeleteView):
+    model = Pengaduan
+    template_name = 'warga/pengaduan_confirm_delete.html'
+    success_url = reverse_lazy('pengaduan-list')
